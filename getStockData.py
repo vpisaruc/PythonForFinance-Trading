da1be2e18a69a34b71f45ba20c0cd9be46a0d384
vpisaruc <vpisaruc@gmail.com>
2019-06-26 15:13:49 +0300

    A little about pandas

--- getStockData.py
+++ getStockData.py
@@ -6,8 +6,15 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2016, 12,  31)
+# start = dt.datetime(2000, 1, 1)
+# end = dt.datetime(2016, 12,  31)
+#
+# df = web.DataReader('TSLA', 'yahoo', start, end)
+# df.to_csv(r'Datasets\tsla.csv')
 
-df = web.DataReader('TSLA', 'yahoo', start, end)
-df.to_csv(r'Datasets\tsls.csv')
+df = pd.read_csv(r'Datasets\tsla.csv', parse_dates=True, index_col=0)
+df['Adj Close'].plot()
+
+
+
+plt.show()
