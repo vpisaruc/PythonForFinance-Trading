05dd3ded2727c858e588fd9df5797c78cef6f319
vpisaruc <vpisaruc@gmail.com>
2019-06-26 15:50:42 +0300

    Basic Stock data Manipulation

--- getStockData.py
+++ getStockData.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
-
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 style.use('ggplot')
 
-# start = dt.datetime(2000, 1, 1)
-# end = dt.datetime(2016, 12,  31)
-#
-# df = web.DataReader('TSLA', 'yahoo', start, end)
-# df.to_csv(r'Datasets\tsla.csv')
-
 df = pd.read_csv(r'Datasets\tsla.csv', parse_dates=True, index_col=0)
-df['Adj Close'].plot()
 
+# min_periods - для того чтобы перыве 100 значений не были нулами
+# первые сто значений каждое следующее значение берется средним из предыдущих
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+df.dropna(inplace=True)
+#print(df.head())
 
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
 
 plt.show()
